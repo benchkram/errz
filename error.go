4dd0013f92e4538bd0295471b4eb8578ee0c0178
equanox <matthias@benchkram.de>
2021-10-21 10:35:37 +0200

    only attch one callstack in Fatal()

--- error.go
+++ error.go
@@ -8,7 +8,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Fatal panics on error
+// Fatal panics on error and attaches a stack trace if needed.
 // First parameter of msgs is used each following variadic arg is dropped
 func Fatal(err error, msgs ...string) {
 	if err != nil {
@@ -17,7 +17,11 @@ func Fatal(err error, msgs ...string) {
 			str = msg
 			break
 		}
-		panic(errors.Wrap(err, str))
+
+		if isPlain(err) {
+			err = errors.Wrap(err, str)
+		}
+		panic(err)
 	}
 }
 
@@ -36,22 +40,20 @@ func Recover(errs ...*error) {
 	}
 
 	if r := recover(); r != nil {
-		errmsg := r.(error)
+		err := r.(error)
 
 		// In case of a memory corruption (e.g. nil pointer dereference)
 		// the error does not have a stack trace and therefore one
 		// is added here.
-		cause := errors.Cause(errmsg)
-		sum := fmt.Sprintf("%+v", errmsg)
-		if cause.Error() == sum {
-			errmsg = errors.WithStack(r.(error))
+		if isPlain(err) {
+			err = errors.WithStack(err)
 		}
 
 		// If error cant't bubble up -> log it
 		if e != nil {
-			*e = errmsg
+			*e = err
 		} else {
-			log(errmsg)
+			log(err)
 		}
 	}
 }
@@ -82,4 +84,17 @@ func log(err error, msgs ...string) {
 	// fmt.Printf("sum: %s\n", sum)
 	// fmt.Printf("stack: %s\n", stack)
 	logger.Error(cause, str, logKeyCallstack, stack)
+}
+
+// isPlain returns true when err is a plain error
+// without a stacktrace.
+func isPlain(err error) (plain bool) {
+
+	cause := errors.Cause(err)
+	sum := fmt.Sprintf("%+v", err)
+	if cause.Error() == sum {
+		plain = true
+	}
+
+	return plain
 }
